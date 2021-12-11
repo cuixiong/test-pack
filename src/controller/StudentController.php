bdfbd920f1ef9b2b9786c065d64ae350b7731ca4
cuixiong <798396652@qq.com>
2021-12-11 09:58:01 +0800

    my first package

---
+++ src/controller/StudentController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * User: set (798396652='czx')
+ * createTime: 2021/12/11  9:57
+ * description:
+ */
+
+namespace Huosdk\TestPack\controller;
+class StudentController {
+    public function study() {
+        return "study";
+    }
+}
